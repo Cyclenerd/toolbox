a6ee09e16d3a6098aa437cfc096196b7adfd5541
Nils K <git@nkn-it.de>
2022-04-14 21:14:25 +0200

    google domain

--- myip.sh
+++ myip.sh
@@ -1,32 +1,21 @@
 #!/usr/bin/env bash
 
-# exit_with_failure() outputs and a message before exiting the script.
-function exit_with_failure() {
-	echo
-	echo "$1"
-	echo
+#
+# Get public external IPv4 address of your own local network
+#
+
+# External URL that returns your IP
+MY_GET_IP_URL="https://domains.google.com/checkip"
+#MY_GET_IP_URL="https://ifconfig.me"
+#MY_GET_IP_URL="http://whatismyip.akamai.com/"
+#MY_GET_IP_URL="https://api.ipify.org"
+
+command -v curl >/dev/null 2>&1 || { echo >&2 "'curl' is needed. Please install 'curl'. More details can be found at https://curl.haxx.se/"; exit 1; }
+
+MY_EXTERNAL_IP=$(curl -4 --silent "$MY_GET_IP_URL")
+if [ -z "$MY_EXTERNAL_IP" ]; then
+	echo "!!! ERROR !!! Could not discover current external IPv4 address"
 	exit 9
-}
-
-# command_exists() tells if a given command exists.
-function command_exists() {
-	command -v "$1" >/dev/null 2>&1
-}
-
-if ! command_exists curl; then
-	exit_with_failure "'curl' is needed. Please install 'curl'. More details can be found at https://curl.haxx.se/"
+else
+	echo "$MY_EXTERNAL_IP"
 fi
-
-ip=$(curl -s https://api.ipify.org)
-
-echo
-echo -n "Public IP Address: $ip"
-
-# Onyl macOS 
-if command_exists pbcopy; then
-	echo "$ip" | pbcopy
-	echo -n " (copied to clipboard)"
-fi
-
-echo
-echo
