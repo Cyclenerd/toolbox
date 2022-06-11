faaaf448ef58c97b3ebd764db5357cd273aff4c6
Nils K <git@nkn-it.de>
2022-06-11 12:41:27 +0200

    terraform validate project_id

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -12,6 +12,21 @@ variable "org_id" {
   }
 }
 
+variable "project_id" {
+  type = string
+  nullable = false
+  description = "The project ID for the resources"
+  # https://cloud.google.com/resource-manager/docs/creating-managing-projects#before_you_begin
+  validation {
+    # Must be 6 to 30 characters in length.
+    # Can only contain lowercase letters, numbers, and hyphens.
+    # Must start with a letter.
+    # Cannot end with a hyphen.
+    condition     = can(regex("^[a-z][a-z0-9-]{4,28}[a-z0-9]$", var.project_id))
+    error_message = "Invalid project ID!"
+  }
+}
+
 variable "billing_account_id" {
   type = string
   nullable = false
@@ -28,7 +43,7 @@ variable "target_amount" {
   description = "Set maximum monthly budget amount (currency as in billing account)"
   default = "1000"
   validation {
-    // https://cloud.google.com/billing/docs/reference/budget/rest/v1/billingAccounts.budgets#BudgetAmount
+    # https://cloud.google.com/billing/docs/reference/budget/rest/v1/billingAccounts.budgets#BudgetAmount
     condition     = can(regex("^1[0-9]+$", var.target_amount))
     error_message = "Specify amount as 64-bit signed integer (1 - 10000000..)!"
   }
